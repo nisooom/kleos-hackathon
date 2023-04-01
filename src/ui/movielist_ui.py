b3dd34bb32905d3c22ec1d946aa0627f10f9cd75
StudentCoder04 <sidhucroedde@gmail.com>
2023-04-01 21:52:51 +0530

    Movie card selectable

--- src/ui/movielist_ui.py
+++ src/ui/movielist_ui.py
@@ -1,6 +1,16 @@
 
+from typing import Optional
 from PyQt5 import QtWidgets, QtGui, QtCore
 from .movieClass import Movie
+
+
+# TEJAS RIGHT HERE
+DEFAULT_STYLESHEET = '''
+color: red
+'''
+SELECTED_STYLESHEET = '''
+color: green
+'''
 
 
 class MovieCardItem(QtWidgets.QWidget):
@@ -9,6 +19,7 @@ class MovieCardItem(QtWidgets.QWidget):
         super().__init__(parent)
         self.movie = movie
         self.list = parent
+        self.setStyleSheet(DEFAULT_STYLESHEET)
         
         title_label = QtWidgets.QLabel(movie.title)
         desc_label = QtWidgets.QLabel(movie.desc)
@@ -27,12 +38,23 @@ class MovieCardItem(QtWidgets.QWidget):
 
     def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
         if event.button() == QtCore.Qt.MouseButton.LeftButton:
+            self.list.update_selected(self)
             self.list.movie_clicked.emit(self.movie)
         return super().mousePressEvent(event)
 
 
 class MovieListWidget(QtWidgets.QScrollArea):
     movie_clicked = QtCore.pyqtSignal(Movie)
+
+    def __init__(self):
+        super().__init__()
+        self.selected_card: Optional[MovieCardItem] = None
+
+    def update_selected(self, card: MovieCardItem) -> None:
+        if self.selected_card is not None:
+            self.selected_card.setStyleSheet(DEFAULT_STYLESHEET)
+        self.selected_card = card
+        self.selected_card.setStyleSheet(SELECTED_STYLESHEET)
 
     def update_list(self, movies: list[Movie]):
         widget = QtWidgets.QWidget()
