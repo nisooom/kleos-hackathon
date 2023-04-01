5350577e165b71a3f71d9db72957531ad19e5f88
Tejas Bhovad <tejasbhovad@gmail.com>
2023-04-01 15:30:52 +0530

    updated css main theme

--- src/ui/randomData.py
+++ src/ui/randomData.py
@@ -2,7 +2,7 @@
 from .movieClass import Movie
 
 test_movies = [
-    Movie('Movie -1','8.0', 'Once upon a time there lived a home','link to imdb', ['Comedy', 'Adventure', 'Drama'],'ME',[],'200'),
+    Movie('Movie -1','8.0', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','link to imdb', ['Comedy', 'Adventure', 'Drama'],'ME',[],'200'),
     Movie('Movie -1','8.0', 'Once upon a time there lived a home','link to imdb', ['Comedy', 'Adventure', 'Drama'],'ME',[],'200'),
     Movie('Movie -2','3.0', 'Once upon a time there lived a den','link to imdb', ['Comedy', 'Adventure', 'Drama'],'ME',[],'200'),
     Movie('Movie -3','5.0', 'Once upon a time there lived a wifi','link to imdb', ['Comedy', 'Adventure', 'Drama'],'ME',[],'200'),
